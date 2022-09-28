56f8139e2fe185a8740dbbc70809bcabbbb0e1c2
sizuki159 <luahantinhthu9@gmail.com>
2022-09-29 00:15:20 +0700

    Change Timezone

--- src/main/java/com/hcmute/myanime/MyAnimeApplication.java
+++ src/main/java/com/hcmute/myanime/MyAnimeApplication.java
@@ -3,11 +3,18 @@ package com.hcmute.myanime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 import java.util.Date;
 
 @SpringBootApplication
 public class MyAnimeApplication {
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));   // It will set UTC timezone
+        System.out.println("Spring boot application running in Asia/Ho_Chi_Minh timezone :"+ new Date());   // It will print Asia/Ho_Chi_Minh timezone
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MyAnimeApplication.class, args);
     }
