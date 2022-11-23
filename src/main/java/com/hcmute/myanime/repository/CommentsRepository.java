219fcc6b0f13e5f7771ffdf1e7219c2327a2bd24
IamDieuKhanh2001 <quachdieukhanh@gmail.com>
2022-11-23 17:12:03 +0700

    fix series of comment is duplicated when render

--- src/main/java/com/hcmute/myanime/repository/CommentsRepository.java
+++ src/main/java/com/hcmute/myanime/repository/CommentsRepository.java
@@ -2,26 +2,15 @@ package com.hcmute.myanime.repository;
 
 import com.hcmute.myanime.model.CommentEntity;
 import com.hcmute.myanime.model.EpisodeEntity;
-import com.hcmute.myanime.model.MovieSeriesEntity;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.domain.Pageable;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface CommentsRepository extends JpaRepository<CommentEntity, Integer> {
 
     Long countByEpisodeByEpisodeId(EpisodeEntity episodeEntity);
-
-//        @Query("SELECT c.episodeByEpisodeId.id FROM CommentEntity c ORDER BY c.createAt DESC")
-//    List<Integer> getEpisodeIDCommentRecentWithLimit(Pageable pageable);
     @Query(value = "select distinct * from (select ms.id, ms.name, ms.description, ms.date_aired, ms.total_episode, ms.image, ms.create_at, ms.movie_id\n" +
             "                         from movie_series ms\n" +
             "                                  join episodes e on ms.id = e.series_id\n" +
@@ -30,10 +19,4 @@ public interface CommentsRepository extends JpaRepository<CommentEntity, Integer
             "                         order by c.create_at desc) as newT\n" +
             "LIMIT :limit", nativeQuery = true)
     List<Object[]> getEpisodeIDCommentRecentWithLimit(int limit);
-
-
-//    @Query(value = "select ms from MovieSeriesEntity ms join " +
-//            "ms.episodesById e join " +
-//            "e.commentsById c group by ms.id, c.createAt order by c.createAt")
-//    List<MovieSeriesEntity> getEpisodeIDCommentRecentWithLimit();
 }
